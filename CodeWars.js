4f32f001947d25ca927eaee4b78dfbc38e892ccf
umkament <97944243+umkament@users.noreply.github.com>
2023-05-20 19:22:17 +0300

    Initial commit

---
+++ CodeWars.js
@@ -0,0 +1,12 @@
+//1. Write a function which calculates the average of the numbers in a given list.
+  // Note: Empty arrays should return 0.
+
+function findAverage(array) {
+  const average = array.reduce((a,b)=>a+b,0)/array.length
+  return average;
+}
+// or
+
+function findAverage(array) {
+  return array.length === 0 ? 0 : array.reduce((a,b)=>a+b,0)/array.length
+}
