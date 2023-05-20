5861a59644dbac7e85eeaf5c322ce71529ed7c22
umkament <97944243+umkament@users.noreply.github.com>
2023-05-20 20:40:54 +0300

    Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.

--- CodeWars.js
+++ CodeWars.js
@@ -10,3 +10,20 @@ function findAverage(array) {
 function findAverage(array) {
   return array.length === 0 ? 0 : array.reduce((a,b)=>a+b,0)/array.length
 }
+
+// 2. Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+function evenOrOdd(number) {
+  if(number % 2 === 0)
+  { return 'Even';
+  } else {
+    return 'Odd';
+  }
+};
+
+// or
+
+function even_or_odd(number) {
+  return number % 2 ? "Odd" : "Even"
+}
