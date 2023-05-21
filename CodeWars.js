9b83442e5bd343efc4ab04f3928bddeb78d48120
umkament <97944243+umkament@users.noreply.github.com>
2023-05-21 09:45:48 +0300

    You will write a card counting function.

--- CodeWars.js
+++ CodeWars.js
@@ -80,6 +80,43 @@ function isIsogram4(str){
 }
 
 
+// 4. You will write a card counting function.
+// It will receive a card parameter, which can be a number or a string,
+// and increment or decrement the global count variable according to the card's value (see table).
+// The function will then return a string with the current count and the string Bet if the count is positive,
+// or Hold if the count is zero or negative. The current count and the player's decision (Bet or Hold)
+// should be separated by a single space.
+//
+// Example Outputs: -3 Hold or 5 Bet
+
+let count = 0;
+
+function cc(card) {
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++
+      break;
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+      count--
+      break;
+  }
+  let holdbet = 'Hold'
+  if (count > 0) {
+    holdbet = 'Bet'
+  }
+
+  return count + " " + holdbet;
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
 
 
 
