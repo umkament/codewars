0e3cf95ce81f777d56c8b43d63278a2c2ee5a13d
umkament <97944243+umkament@users.noreply.github.com>
2024-02-27 11:37:57 +0300

    заменяет буквы на противоположные

--- CodeWars.js
+++ CodeWars.js
@@ -325,10 +325,12 @@ console.log(repeatString(3, '6c'))*/
 
 
 
+//Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
 
 
-
-
-
+// function doubleChar(str) {
+//   let arr = str.split('').map(el=>el+el)
+//   return arr.join('')
+// }
 
 
