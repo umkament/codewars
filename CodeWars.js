771a57a8e9fed90c6da71de8ab4b711b5ce7322a
umkament <97944243+umkament@users.noreply.github.com>
2024-02-27 11:08:05 +0300

    массив слов в предложение

--- CodeWars.js
+++ CodeWars.js
@@ -283,7 +283,14 @@ console.log(repeatString(3, '6c'))*/
 
 // const quarterOf = m => Math.ceil(m/3);
 
+//Write a function that takes an array of words and smashes them together
+// into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation,
+// but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning
+// or the end of the sentence!
 
+function smash (words) {
+  return words.join(' ')
+};
 
 
 
