51e9286cde9fe0f4dab6efe8c8f281fca79c15e4
umkament <97944243+umkament@users.noreply.github.com>
2024-03-04 23:04:50 +0300

    циклы

--- CodeWars.js
+++ CodeWars.js
@@ -337,4 +337,36 @@ console.log(repeatString(3, '6c'))*/
 
 // const doubleChar = (str) => str.split("").map(c => c + c).join("");
 
+/*Our football team has finished the championship.
 
+   Our team's match results are recorded in a collection of strings.
+   Each match is represented by a string in the format "x:y", where x is our team's score and y is our opponents score.
+
+   For example: ["3:1", "2:2", "0:1", ...]
+
+Points are awarded for each match as follows:
+
+   if x > y: 3 points (win)
+if x < y: 0 points (loss)
+if x = y: 1 point (tie)
+We need to write a function that takes this collection and returns the number of points our team (x)
+got in the championship by the rules given above.
+
+   Notes:
+
+our team always plays 10 matches in the championship
+0 <= x <= 4
+0 <= y <= 4*/
+
+// function points(games) {
+//   let allPoints = 0;
+//   for (let i=0; i<games.length; i++) {
+//     const [x, y] = games[i].split(':');
+//     if (x>y) {
+//       allPoints += 3;
+//     } else if (x===y){
+//       allPoints +=1;
+//     }
+//   }
+//   return allPoints
+// }
