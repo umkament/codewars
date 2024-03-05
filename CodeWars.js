28eb42c863c02f08e60c3ae764283dea363681ad
umkament <97944243+umkament@users.noreply.github.com>
2024-03-05 11:54:27 +0300

    работа со строками

--- CodeWars.js
+++ CodeWars.js
@@ -464,3 +464,27 @@ You might know some pretty large perfect squares. But what about the NEXT one?
 function findNextSquare(sq) {
   return Math.sqrt(sq) % 1 ? -1 : Math.pow(Math.sqrt(sq) + 1, 2);
 }
+
+/*
+Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string,
+the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+   Examples:
+
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"*/
+
+function longest(s1, s2) {
+  let newStr = (s1+s2).split('')
+  let newArr = [...new Set(newStr)]
+  return newArr.sort().join('')
+}
+
+//короткая запись
+function longest1(s1, s2) {
+  return [...new Set((s1+s2).split(''))].sort().join('')
+}
