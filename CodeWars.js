19d7ecb8029030e5ef5c2c377478cad8e52963f2
umkament <97944243+umkament@users.noreply.github.com>
2024-03-10 00:48:56 +0300

    сравниваем два массива между собой

--- CodeWars.js
+++ CodeWars.js
@@ -703,3 +703,70 @@ function count1 (string) {
   return count;
 }
 
+/*Given two arrays a and b write a function comp(a, b) (orcompSame(a, b))
+that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
+Examples
+
+Valid arrays
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]
+b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144,
+361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]
+b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+Invalid arrays
+
+If, for example, we change the first number to something else, comp is not returning true anymore:
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]
+b = [132, 14641, 20736, 361, 25921, 361, 20736, 361]
+comp(a,b) returns false because in b 132 is not the square of any number of a.
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]
+b = [121, 14641, 20736, 36100, 25921, 361, 20736, 361]
+comp(a,b) returns false because in b 36100 is not the square of any number of a.
+
+Remarks
+
+a or b might be [] or {} (all languages except R, Shell).
+a or b might be nil or null or None or nothing (except in C++, COBOL, Crystal, D, Dart, Elixir,
+Fortran, F#, Haskell, Nim, OCaml, Pascal, Perl, PowerShell, Prolog, PureScript, R, Racket, Rust, Shell, Swift).
+If a or b are nil (or null or None, depending on the language), the problem doesn't make sense so return false.*/
+
+//1й вариант
+function comp(a, b){
+  if(!a || !b || a.length !== b.length){
+    return false
+  }
+  a = a.map (x=>x*x)
+
+  a.sort((x,y)=>x-y)
+  b.sort((x,y)=>x-y)
+
+ for (let i=0; i<a.length; i++) {
+   if (a[i] !== b[i]){
+     return false
+   }
+ }
+
+  return true
+}
+
+//2-й вариант
+function comp2(a, b){
+  if(!a || !b || a.length !== b.length){
+    return false
+  }
+  a = a.map (x=>x*x).sort().join()
+  b.sort().join()
+  return a === b
+}
+
+//краткая запись
+
+function comp1(a, b){
+  return !!a && !!b && a.map(x=>x*x).sort().join() === b.sort().join()
+}
