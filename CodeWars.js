f8cf500edce8bdde1f5994e11fdb7439b0cd7648
umkament <97944243+umkament@users.noreply.github.com>
2024-03-14 13:52:55 +0300

    сортировка чисел целого числа

--- CodeWars.js
+++ CodeWars.js
@@ -936,3 +936,23 @@ function nbYear1(p0, percent, aug, p) {
   }
   return years
 }
+
+/*Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+Examples:
+
+Input: 42145 Output: 54421
+
+Input: 145263 Output: 654321
+
+Input: 123456789 Output: 987654321*/
+
+function descendingOrder(n){
+  return parseInt(n.toString().split('').sort((a,b)=>b-a).join(''))
+}
+
+//пишут что лучшая практика с добавлением reverse()
+
+function descendingOrder1(n){
+  return parseInt(String(n).split('').sort().reverse().join(''))
+}
