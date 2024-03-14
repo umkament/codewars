72fefc2e7894b293ce9e5e771c65c0f3572d4afb
umkament <97944243+umkament@users.noreply.github.com>
2024-03-14 13:37:16 +0300

    вычисляли количество лет, за которое вырастает население

--- CodeWars.js
+++ CodeWars.js
@@ -926,3 +926,13 @@ function nbYear(p0, percent, aug, p) {
   }
   return n
 }
+
+//best practice
+
+function nbYear1(p0, percent, aug, p) {
+
+  for (var years = 0; p0 < p; years++) {
+    p0 = Math.floor(p0 + p0 * percent / 100 + aug);
+  }
+  return years
+}
