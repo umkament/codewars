2ccfa563eb48b77cc6b80f2581de4d379d73b0d0
umkament <97944243+umkament@users.noreply.github.com>
2024-03-18 18:16:22 +0300

    считаем символы в строке

--- CodeWars.js
+++ CodeWars.js
@@ -1023,3 +1023,38 @@ function alphabetPosition(text) {
   }
   return result.trim()
 }
+
+/*Count the number of Duplicates
+
+Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+Example
+
+"abcde" -> 0  # no characters repeats more than once
+"aabbcde" -> 2  # 'a' and 'b'
+"aabBcde" -> 2  # 'a' occurs twice and 'b' twice (`b` and `B`)
+"indivisibility" -> 1  # 'i' occurs six times
+"Indivisibilities" -> 2  # 'i' occurs seven times and 's' occurs twice
+"aA11" -> 2  # 'a' and '1'
+"ABBA" -> 2  # 'A' and 'B' each occur twice*/
+
+function duplicateCount(text){
+  text = text.toLowerCase()
+  obj={}
+  count = 0
+  for (el of text){
+    obj[el] = (obj[el] || 0) + 1
+    if (obj[el] === 2) {
+      count++
+    }
+  }
+  return count
+}
+
+//второй вариант решения
+
+function duplicateCount1(text){
+  return text.toLowerCase().split('').filter(function(val, i, arr){
+    return arr.indexOf(val) !== i && arr.lastIndexOf(val) === i;
+  }).length;
+}
