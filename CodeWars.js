f8d7bc7b7df650be8823def1442efb19e14b9dc5
umkament <97944243+umkament@users.noreply.github.com>
2024-03-18 17:09:18 +0300

    возвращаем числа вместо букв

--- CodeWars.js
+++ CodeWars.js
@@ -998,3 +998,28 @@ number of dragons, will he survive?
 Return true if yes, false otherwise :)*/
 
 var hero = (bullets, dragons) => bullets/dragons>=2;
+
+/*Welcome.
+
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+
+Example
+
+alphabetPosition("The sunset sets at twelve o' clock.")
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )*/
+
+function alphabetPosition(text) {
+  var result = ''
+  text = text.toLowerCase()
+  for (i=0; i<text.length; i++){
+    var letterNumber = text.charCodeAt(i)
+    if (letterNumber >=97 && letterNumber<=122){
+      result += (letterNumber - 96) + ' '
+    }
+  }
+  return result.trim()
+}
