6bd67fe88f1c3007ca20d1954a568ca35dd2cd5a
umkament <97944243+umkament@users.noreply.github.com>
2024-03-20 18:44:43 +0300

    трибоначчи

--- CodeWars.js
+++ CodeWars.js
@@ -1218,3 +1218,11 @@ function tribonacci(signature,n){
   }
   return arr
 }
+
+//короткий вариант решения без создания копий массива
+function tribonacci1(signature,n){
+  for (var i = 0; i < n-3; i++) {
+    signature.push(signature[i] + signature[i+1] + signature[i+2]);
+  }
+  return signature.slice(0, n);
+}
