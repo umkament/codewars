5370c0a975775f93e1ad8e2349b641d6604cea29
umkament <97944243+umkament@users.noreply.github.com>
2024-03-26 12:56:04 +0300

    проверяем число на четность

--- CodeWars.js
+++ CodeWars.js
@@ -1300,3 +1300,87 @@ function switchItUp1(number) {
 // более коротка и правильная запись через массив,
 
 switchItUp3=n=>["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"][n]
+
+/*You live in the city of Cartesia where all roads are laid out in a perfect grid.
+You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk.
+ The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button
+ it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']).
+ You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse
+ one city block, so create a function that will return true if the walk the app gives you will take you exactly
+ ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point.
+ Return false otherwise.
+
+Note: you will always receive a valid array containing a random assortment of direction letters
+('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).*/
+
+//мой вариант с фильтром
+function isValidWalk(walk) {
+  if (walk.length !== 10){
+    return false
+  }
+  return walk.filter(el => el === 'n').length === walk.filter(el => el === 's').length &&
+     walk.filter(el => el === 'w').length === walk.filter(el => el === 'e').length;
+
+}
+
+//решение с осями координат, чтобы значение по осям оставалось [0,0]
+
+function isValidWalk1(walk) {
+  var dx = 0
+  var dy = 0
+  var dt = walk.length
+
+  for (var i = 0; i < walk.length; i++) {
+    switch (walk[i]) {
+      case 'n': dy--; break
+      case 's': dy++; break
+      case 'w': dx--; break
+      case 'e': dx++; break
+    }
+  }
+
+  return dt === 10 && dx === 0 && dy === 0
+}
+
+/*In this Kata we are passing a number (n) into a function.
+
+Your code will determine if the number passed is even (or not).
+
+The function needs to return either a true or false.
+
+Numbers may be positive or negative, integers or floats.
+
+Floats with decimal part non equal to zero are considered UNeven for this kata.*/
+
+//проверяем число на четность
+
+var testEven = (n) => n%2 === 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
